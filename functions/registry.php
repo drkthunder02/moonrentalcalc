1cf39540d1a0bee74818e79b140267db3c141253
Chris Mancuso <mancuso.christopher@gmail.com>
2018-04-09 22:33:29 -0500

    added autoload

--- functions/registry.php
+++ functions/registry.php
@@ -6,6 +6,8 @@
  * and open the template in the editor.
  */
 
+require_once __DIR__.'/../vendor/autoload.php';
+
 require_once __DIR__.'/../functions/database/dbopen.php';
 require_once __DIR__.'/../functions/database/dbclose.php';
 require_once __DIR__.'/../functions/html/printhtmlheader.php';
